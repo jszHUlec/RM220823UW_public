ba532bb9e34a61eb111c4c5435505debd6a56e25
JS Lecturer <jakub.sz@hackeru.com>
2023-12-14 14:27:56 -0500

    wykonane w srode - redteam HackerU

--- tydizen_2/zadanie30.py
+++ tydizen_2/zadanie30.py
@@ -2,3 +2,24 @@
 # pobierz strone 'https://archiwum.mz.gov.pl/zdrowie-i-profilaktyka/grypa/materialy-do-pobrania/'
 # wyciagnij wszystkie elementy 'a' z linkiem do pliku '.pdf' tej strony
 # pobierz plik 'pdf' na swoj komputer
+
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = "https://archiwum.mz.gov.pl/zdrowie-i-profilaktyka/grypa/materialy-do-pobrania/"
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        if link.endswith('.pdf'):
+            pdf_url = link
+            pdf_filename = os.path.basename(pdf_url)
+            pdf_content = requests.get(pdf_url).content
+            with open(pdf_filename, 'wb') as pdf_file:
+                pdf_file.write(pdf_content)
+                print(f"Downloaded: {pdf_filename}")
+else:
+    print(f"Error: {response.status_code}")
